4f7794230d67c3656afa7381eae0d8a4941bbbd9
Lorina Knippfeld <lorina.knippfeld@t-online.de>
2021-03-07 14:35:49 +0100

    axios & welcome

--- client/src/register.js
+++ client/src/register.js
@@ -1,4 +1,9 @@
+// import React & axios
+
 import React from "react";
+import axios from "axios";
+
+// set up the class component
 
 export default class Register extends React.Component {
     constructor() {
@@ -12,15 +17,46 @@ export default class Register extends React.Component {
         };
     }
 
-    handleChange = (event) => {
+    // react on changes for states
+
+    handleChange(event) {
         this.setState({
             [event.target.name]: event.target.value,
         });
-    };
+    }
 
-    onSubmit = (event) => {
+    // prevent default form behaviour
+
+    onSubmit(event) {
         event.preventDefault();
-    };
+    }
+
+    // make axios request on clicking the button
+
+    submit() {
+        const user = {
+            firstname: this.state.firstname,
+            lastanme: this.state.lastname,
+            email: this.state.email,
+            password: this.state.password,
+        };
+        axios
+            .post("/api/register", { user })
+            .then((response) => {
+                console.log("reponse fom register axios", response.data);
+
+                if (response.data.success) {
+                    location.replace("/");
+                } else {
+                    this.setState({ error: response.data.error });
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    // render the form
 
     render() {
         return (
--- client/src/welcome.js
+++ client/src/welcome.js
@@ -4,21 +4,12 @@ import Register from "./register.js";
 export default class Welcome extends React.Component {
     constructor() {
         super();
-        this.state = {
-            name: "",
-        };
-    }
-
-    handleChange(event) {
-        this.setState({ name: event.target.value });
     }
 
     render() {
-        const { name } = this.state;
         return (
-            <div>
-                <h2>Welcome {name} !</h2>
-                <input onChange={(event) => this.handleChange(event)}></input>
+            <div className="register">
+                <Register />
             </div>
         );
     }
